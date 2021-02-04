2544afba8cb09308675a47c99446626a0ab0f6c4
Oleg Lihogub <oligli@yandex.ru>
2021-02-04 21:53:11 +0300

    implemented three args support

--- arg.c
+++ arg.c
@@ -1,10 +1,12 @@
-//puts first command line argument as integer to RDI register
+//puts 0-3 argument as integer to RDI, RSI,  register
 #include <stdio.h>
 
-extern void func(int f);
+extern void func(int, int, int);
 
 int main(int argc, char* argv[]) {
-    int arg = atoi(argv[1]);
-    func(arg);
+    int arg1 = (argc > 1) ? atoi(argv[1]) : 0;
+    int arg2 = (argc > 2) ? atoi(argv[2]) : 0;
+    int arg3 = (argc > 3) ? atoi(argv[3]) : 0;
+    func(arg1, arg2, arg3);
     return 0;
 }
