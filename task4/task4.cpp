03429918a4ed0312858e6aec779366670008384c
Oleg Lihogub <oligli@yandex.ru>
2021-02-25 00:04:13 +0300

    fixed task4

--- task4/task4.cpp
+++ task4/task4.cpp
@@ -15,22 +15,18 @@ int main() {
 
     cout << "F truth table:" << endl;
     for (int i = 0; i < (1 << n); i++) {
-        for (int j = 0; j < (n+1); j ++) {
-            cin >> c;
-            F[i] <<= 1;
-            F[i] += c - '0';
-        }
+        cin >> c;
+        F[i] <<= 1;
+        F[i] += c - '0';
     }
 
     bitArray* G = new bitArray[1 << n];
 
     cout << "G truth table:" << endl;
     for (int i = 0; i < (1 << n); i++) {
-        for (int j = 0; j < (n+1); j ++) {
-            cin >> c;
-            G[i] <<= 1;
-            G[i] += c - '0';
-        }
+        cin >> c;
+        G[i] <<= 1;
+        G[i] += c - '0';
     }
 
     bitArray* H = new bitArray[1 << (2*n-1)];
@@ -39,12 +35,7 @@ int main() {
 
     cout << "F(G) truth table:" << endl;
     for (int i = 0; i < (1<<(2*n-1)); i++) {
-        bitArray mask = (1<<(2*n-1));
-        for (int j = 0; j < (2*n); j++) {
-            cout << (((H[i] & mask) > 0) ? 1 : 0);
-            mask >>= 1;
-        }
-        cout << endl;
+        cout << (H[i]%2) << endl;
     }
 
     return 0;
